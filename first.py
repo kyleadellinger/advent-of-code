ef8cffeb528c05eca6ac177960d0c511979fccf1
kydell <gogointerweb@gmail.com>
2023-06-05 21:18:49 -0400

    first part, two

--- first.py
+++ first.py
@@ -9,4 +9,8 @@ for line in calories:
         temp_count = 0
     elif line != "\n":
         temp_count += int(line.strip("\n"))
-#print(max(cal_list))
+print(max(cal_list))
+
+cal_list.sort()
+top_three = [int(cal_list[-1]), int(cal_list[-2]), int(cal_list[-3])]
+print(sum(top_three))
